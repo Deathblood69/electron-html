9f57c9fa0030e54744cc4bbf960280fb124e8791
MaximusCrowe <deathblood69002@gmail.com>
2024-10-31 01:02:33 +0100

    feat(window): Maximiser la fenêtre à l'ouverture de l'application et cacher la barre de menu

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-// main.js
-
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('node:path')
 
-const createWindow = () => {
+const createWindow = async () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        show: false,
+        autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
+    mainWindow.maximize()
+    mainWindow.show()
+
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+    await mainWindow.loadFile('index.html')
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -24,13 +24,15 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-    createWindow()
+app.whenReady().then(async () => {
+    await createWindow()
 
-    app.on('activate', () => {
+    app.on('activate', async () => {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) {
+            await createWindow()
+        }
     })
 })
 
